21d89642fd05869a9b54f927dd16cc0f13ea6984
nocturne47 <immanuelnaibaho1@gmail.com>
2023-06-29 21:45:41 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, dignissimos accusamus perferendis culpa vero inventore? Facilis, quasi numquam! Maiores possimus ex totam esse ducimus veniam cum ad illo nulla libero.</p>
 </body>
 </html>
